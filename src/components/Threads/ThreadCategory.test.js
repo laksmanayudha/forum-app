1fa02f470b7c9af01862f8a2266531969117f333
laksmanayudha <laksmanayudha22@gmail.com>
2023-01-07 01:00:25 +0800

    createt test for ThreadCategory and RegisterPage

---
+++ src/components/Threads/ThreadCategory.test.js
@@ -0,0 +1,32 @@
+/**
+ * test scenario ThreadCategory Component
+ *
+ * should call action function when user clicked
+ * should show indication when actived
+ */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import ThreadCategory from './ThreadCategory';
+
+describe('ThreadCategory Component', () => {
+  it('should handle user click correctly', async () => {
+    // arrange
+    const action = jest.fn();
+    render(<ThreadCategory label="categoryLabel" active={false} action={action} />);
+    const threadCategory = await screen.getByText('categoryLabel');
+    // action
+    await userEvent.click(threadCategory);
+    // assert
+    expect(action).toHaveBeenCalledWith(false, 'categoryLabel');
+  });
+
+  it('should show indication when actived', async () => {
+    // arrange
+    render(<ThreadCategory label="categoryLabel" active action={() => {}} />);
+    const threadCategory = await screen.getByText('categoryLabel');
+    // assert
+    expect(threadCategory).toHaveClass('category--active');
+  });
+});
--- src/pages/RegisterPage.test.js
+++ src/pages/RegisterPage.test.js
@@ -1,9 +1,10 @@
 /**
- * test scenario for LoginPage Component
+ * test scenario for RegisterPage Component
  *
+ * should handle user input name correctly
  * should handle user input email correctly
  * should handle user input password correctly
- * should call login api when login button is clicked
+ * should call register api when register button is clicked
  */
 import React from 'react';
 import { Provider } from 'react-redux';
@@ -12,18 +13,27 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import store from '../states';
-import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
 import api from '../utils/api';
 
-describe('LoginPage Component', () => {
+describe('RegisterPage Component', () => {
   beforeEach(() => {
     render(
       <BrowserRouter>
         <Provider store={store}>
-          <LoginPage />
+          <RegisterPage />
         </Provider>
       </BrowserRouter>,
     );
+  });
+
+  it('handle user input name correctly', async () => {
+    // arrange
+    const nameInput = await screen.getByPlaceholderText('Your Name');
+    // action
+    await userEvent.type(nameInput, 'userName');
+    // assert
+    expect(nameInput).toHaveValue('userName');
   });
 
   it('handle user input email correctly', async () => {
@@ -44,17 +54,21 @@ describe('LoginPage Component', () => {
     expect(passwordInput).toHaveValue('userPassword');
   });
 
-  it('should call login api when login button is clicked', async () => {
-    const spy = jest.spyOn(api, 'login');
+  it('should call register api when register button is clicked', async () => {
+    // arrange
+    const spy = jest.spyOn(api, 'register');
+    const nameInput = await screen.getByPlaceholderText('Your Name');
     const emailInput = await screen.getByPlaceholderText('Your Email');
     const passwordInput = await screen.getByPlaceholderText('Your Password');
-    const loginButton = await screen.getByRole('button', { name: 'Login' });
+    const registerButton = await screen.getByRole('button', { name: 'Register' });
     // action
+    await userEvent.type(nameInput, 'userName');
     await userEvent.type(emailInput, 'userEmail');
     await userEvent.type(passwordInput, 'userPassword');
-    await userEvent.click(loginButton);
+    await userEvent.click(registerButton);
     // assert
     expect(spy).toHaveBeenCalledWith({
+      name: 'userName',
       email: 'userEmail',
       password: 'userPassword',
     });

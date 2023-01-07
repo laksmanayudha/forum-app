b7ab551cd8188fb908667ee9fbfac31f1065f615
laksmanayudha <laksmanayudha22@gmail.com>
2023-01-07 20:42:37 +0800

    make test pass

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function useInput() {
   const [input, setInput] = useState('');
   const onInputChange = (value) => {
-    setInput(`${value}maketesterror`);
+    setInput(value);
   };
   return [input, onInputChange];
 }
